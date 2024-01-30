b3b73e0c4a23687b8664a433b4a6fa6655000c0e
emayom <emayom7@gmail.com>
2024-01-30 09:36:09 +0900

    :wrench: Chore: resolve.alias 옵션 추가

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: "@", replacement: resolve(__dirname, "src") },
+      {
+        find: "@assets",
+        replacement: resolve(__dirname, "src/assets"),
+      },
+      {
+        find: "@components",
+        replacement: resolve(__dirname, "src/components"),
+      },
+    ],
+  },
+});
