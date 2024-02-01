bd2e6f0e9b220372ecf5664b6df377bf61fa4a99
emayom <emayom7@gmail.com>
2024-02-02 01:44:25 +0900

    :sparkles: Feat: 라우터 추가 및 페이지 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+
+import Main from "./pages/Main";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
