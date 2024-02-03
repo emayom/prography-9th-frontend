438d5e6d0b8139fe4c0c2cad170a1a95f6c58ff5
emayom <emayom7@gmail.com>
2024-02-04 04:48:39 +0900

    :sparkles: Feat: LazyImage 로드 완료 이후 이미지 보여주도록 수정

--- src/components/LazyImage/index.jsx
+++ src/components/LazyImage/index.jsx
@@ -1,13 +1,23 @@
+import { useState } from "react";
 import useIntersection from "@hooks/useIntersection";
 
 const LazyImage = ({ src, alt, ...props }) => {
+  const [loaded, setLoaded] = useState(false);
   const imageRef = useIntersection(
     () => {
-      imageRef.current.src = src;
+      if (!loaded) {
+        const img = new Image();
+        img.src = src;
+        img.onload = () => {
+          setLoaded(true);
+        };
+      }
     },
     { threshold: 0.2 }
   );
-  return <img ref={imageRef} alt={alt} {...props} />;
+  return (
+    <div ref={imageRef}>{loaded && <img src={src} alt={alt} {...props} />}</div>
+  );
 };
 
 export default LazyImage;
