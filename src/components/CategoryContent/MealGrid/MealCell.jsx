a9c34ccbebfeaffc3cc086976029569c122d3063
emayom <emayom7@gmail.com>
2024-02-04 04:49:44 +0900

    :recycle: Fix: 기존 img 태그 LazyImage 컴포넌트로 수정

--- src/components/CategoryContent/MealGrid/MealCell.jsx
+++ src/components/CategoryContent/MealGrid/MealCell.jsx
@@ -1,26 +1,17 @@
-import { useState, useEffect, memo } from "react";
+import { memo } from "react";
 
 import Text from "@components/Text";
+import LazyImage from "@components/LazyImage";
 
 const MealCell = memo(function MealCell({ altText, imageUrl }) {
-  const [loaded, setLoaded] = useState(false);
-
-  useEffect(() => {
-    const img = new Image();
-    img.src = imageUrl;
-    img.onload = () => setLoaded(true);
-  }, [imageUrl]);
-
   return (
     <div className="group relative flex flex-col">
       <div className="relative overflow-hidden bg-stone-100 shadow-md rounded-md pb-[100%]">
-        {loaded && (
-          <img
-            src={imageUrl}
-            alt={altText}
-            className="absolute transition ease-in-out delay-150 overflow-hidden group-hover:scale-110 will-change-transform"
-          />
-        )}
+        <LazyImage
+          src={imageUrl}
+          alt={altText}
+          className="absolute transition ease-in-out delay-150 overflow-hidden group-hover:scale-110 will-change-transform"
+        />
       </div>
       <Text
         component={"span"}
