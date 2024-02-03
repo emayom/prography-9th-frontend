17cfe6673acd62bcf027dad357041b5ac6ffe346
emayom <emayom7@gmail.com>
2024-02-04 06:00:31 +0900

    :sparkles: Feat: CategoryProvider 추가

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClientProvider, QueryClient } from "react-query";
 
+import { CategoryProvider } from "./context/CategoryProvider.jsx";
+
 import App from "./App.jsx";
 import "./index.css";
 
@@ -12,7 +14,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <CategoryProvider>
+          <App />
+        </CategoryProvider>
       </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
