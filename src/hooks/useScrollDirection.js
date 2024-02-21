78a88255188a3ce0843f68bb967b86f33292a54c
emayom <emayom7@gmail.com>
2024-02-21 23:32:47 +0900

    :recycle: Refactor: setDirection 이전에 확인 조건문 추가
    direction이 같을 경우 state를  업데이트하지 않도록 개선하였습니다.

--- src/hooks/useScrollDirection.js
+++ src/hooks/useScrollDirection.js
@@ -6,12 +6,19 @@ const useScrollDirection = () => {
   const prevScrollY = useRef(0);
   const handleScroll = useMemo(
     () =>
+      /**
+       * debounce 대신 throttle를 사용한 이유:
+       * 해당 훅은 실시간 스크롤 방향 변화를 제공하기 위한 목적으로 작성되었습니다.
+       * 사용자의 스크롤 이벤트의 마지막이 아닌 스크롤 이벤트 시작 방향을 빠르게 전달하여 로직을 처리할 수 있도록 구현되었습니다.
+       */
       throttle(() => {
-        setDirection(prevScrollY.current < window.scrollY ? "Down" : "Up");
+        const current = prevScrollY.current < window.scrollY ? "Down" : "Up";
+
+        if (current !== direction) setDirection(current);
 
         prevScrollY.current = window.scrollY;
       }, 250),
-    [prevScrollY]
+    [prevScrollY, direction]
   );
 
   useEffect(() => {
